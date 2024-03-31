b0244751495025a321bcff76ceafd1a1ff46023d
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-03-31 12:53:15 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// 1 MASALA
+// class car {
+//   constructor(brand, year) {
+//     this.brand = brand;
+//     this.year = year;
+//   }
+// }
+// let mashina = new car("damas legenda", 2010);
+// let mashina2 = new car("MERS", 2000);
+// console.log(mashina, mashina2);
+
+// 2 MASALA
+// class car {
+//   constructor(brand, year) {
+//     this.brand = brand;
+//     this.year = year;
+//   }
+// }
+// class ElectricCar extends car {
+//   constructor(brand, year, ElectricCar) {
+//     super(brand, year);
+//     this.ElectricCar = ElectricCar;
+//   }
+// }
+// let mashina = new car("damas legenda", 2010);
+// let elektircar = new ElectricCar("tesla", 2015, 10000);
+// console.log(mashina, elektircar);
+
+// 3 MASALA
+// class car {
+//   constructor(brand, year) {
+//     this.brand = brand;
+//     this.year = year;
+//   }
+//   displayinfo() {
+//     console.log(
+//       `Mashina markasi ${this.brand} va ishlab chiqarilgan yil ${this.year}`
+//     );
+//   }
+// }
+// let mashina = new car("damas legenda", 2010);
+// mashina.displayinfo();
+
+// class ElectricCar extends car {
+//   constructor(brand, year, ElectricCar) {
+//     super(brand, year);
+//     this.ElectricCar = ElectricCar;
+//   }
+// }
+// let elektircar = new ElectricCar("tesla", 2015, 10000);
+
+// elektircar.displayinfo();
